18e02674cb40efe5a7982d1d5c23550eaec4dee3
tahsina-azam <tahsina.sheeva@gmail.com>
2024-08-25 11:24:48 +0600

    fix: build errors

--- pages/ImageComponent.tsx
+++ pages/ImageComponent.tsx
@@ -1,29 +1,41 @@
-import React from "react";
+// import React from "react";
 
-export const ImageComponent = () => {
+// export const ImageComponent = () => {
 
-    const markers = [
-      { name: 'Forge', x: 50.4, y: 26 },
-      { name: 'Rentals', x: 58.5, y: 76.3 },
-      // Add more 
-    ];
+//     const markers = [
+//       { name: 'Forge', x: 50.4, y: 26 },
+//       { name: 'Rentals', x: 58.5, y: 76.3 },
+//       // Add more 
+//     ];
   
-    const handleMarkerClick = (m) => {
-      // do something with marker
-    };
+//     const handleMarkerClick = (m: { name: string; x: number; y: number; }) => {
+//       // do something with marker
+//     };
   
-    return (
-      <div style={{ position: 'relative', width: '100vw' }}>
-        <img style={{ width: '100%', display: 'block'}} src="/assets/img.jpg" alt="Image with markers" />
+//     return (
+//       <div style={{ position: 'relative', width: '100vw' }}>
+//         <img style={{ width: '100%', display: 'block'}} src="/assets/img.jpg" alt="Image with markers" />
   
-        {markers.map((marker, index) => (
-          <div
-            key={index}
-            className="marker"
-            style={{ left: `${marker.x}%`, top: `${marker.y}%`, position: "absolute" }}
-            onClick={() => handleMarkerClick(marker)}
-          />
-        ))}
-      </div>
-    );
-  };
+//         {markers.map((marker, index) => (
+//           <div
+//             key={index}
+//             className="marker"
+//             style={{ left: `${marker.x}%`, top: `${marker.y}%`, position: "absolute" }}
+//             onClick={() => handleMarkerClick(marker)}
+//           />
+//         ))}
+//       </div>
+//     );
+//   };
+
+import React from 'react'
+
+function ImageComponent() {
+  return (
+    <div>
+      image component
+    </div>
+  )
+}
+
+export default ImageComponent
