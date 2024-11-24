b662dcbadb4ca959044b8064e5e274ebe4b4581b
tahsina-azam <tahsina.sheeva@gmail.com>
2024-11-24 16:48:49 +0600

    test: line height fix with 1.6 vh

--- components/TextContent.jsx
+++ components/TextContent.jsx
@@ -27,8 +27,8 @@ import * as React from "react";
 
 const TextContent = ({ text }) => (
   <div className="w-[13.4375vw]"> {/* 258px = 13.4375vw */}
-    <p className="font-s-pro z-10 uppercase text-[0.78vw] tracking-[-0.04688vw]  text-black font-[274]"  style={{
-        lineHeight: "16px", // Adjust to match your design
+    <p className="font-s-pro z-10 uppercase text-[0.78vw] tracking-[-0.063vw] text-black font-[274]"  style={{
+        lineHeight: "1.6vh", // Adjust to match your design
         WebkitFontSmoothing: "subpixel-antialiased", // Match Safari behavior
         MozOsxFontSmoothing: "grayscale", // Match Firefox behavior
       }}> {/* 16px = 0.833vw; tracking -0.9px = -0.04688vw */}
